22ac890d23b7885eb67085cf0fcf9c1e3991cb38
Aqqula <batanova.m@bk.ru>
2020-10-22 19:52:28 +0300

    Day4
    complete

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './components/App.vue'
+import Students from './components/Students.vue'
+import StudentInfo from './components/StudentInfo.vue'
+
+const routes = [
+   { path: '/', component: Students},
+   { path: '/student-info/:id', component: StudentInfo, props: true}
+]
+
+const router = new VueRouter({
+   routes
+})
+
+Vue.use(VueRouter)
+Vue.use(VueAxios,axios)
 
 new Vue({
    render: h => h(App),
    el: '#App',
+   router
 })
